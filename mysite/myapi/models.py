185b32c452f695dce10fd7f98c2ef7c105c086d0
Andy <potterfield2005@gmail.com>
2022-03-02 22:53:07 -0600

    Add new hero with ajax

--- mysite/myapi/models.py
+++ mysite/myapi/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Hero(models.Model):
     name = models.CharField(max_length=60)
-    alias = models.CharField(max_length=60)
+    alias = models.CharField(max_length=60, blank=True, null=True)
 
     def __str__(self):
         return self.name
